ce6c6e9198cd9ebf3fe5141b27e765269f568463
Qazi-Saad <saadbinrafay@gmail.com>
2022-12-31 14:07:05 +0500

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /******************************** Section 2 **************************/ 
 
-/************  Q#1: Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.                                  [Marks: 15] *****************/
+/************  Q#1: Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.                       [Marks: 15] *****************/
 
 var library = [ 
     {
@@ -32,7 +32,7 @@ for (var i = 0; i < library.length; i++)
     }
    }
 
-/****** Q#2: Create a program that creates 3 Employee objects from an Employee class with ID, Name, Department and Salary properties. Values of properties should be assigned through getter and setter methods (ES6). Display all the properties of all 3 employees.                                                [Marks: 15] ****/    
+/****** Q#2: Create a program that creates 3 Employee objects from an Employee class with ID, Name, Department and Salary properties. Values of properties should be assigned through getter and setter methods (ES6). Display all the properties of all 3 employees.                       [Marks: 15] ****/    
 
 class employee {
     setValues(id, name, dept, salary) {
@@ -63,6 +63,41 @@ object3.setValues(5942, 'Jawad', 'Development', 70000);
 object3.getValues();
 
 /*** Q#3: At times, when we access a JavaScript object, we receive raw JSON string and we need to convert it to an object ourselves to use it in our program. Similarly, we also may require an object in JSON string form. Write a program that parses JSON string in ‘member’ array from following link to an object and display its contents. You are also required to stringify the last object in ‘member’ array.  
-https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json                  [Marks: 15] 
+https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json           [Marks: 15] 
  ****/ 
+
+
+
+
+/******************************** Section 3 **************************/ 
+
+/**** Q#3: Write a program that takes a number of greater than 5 digits from input field and shows sum of all odd numbers. For example, if the input is 196783, the sum would 
+            be 1 + 9 + 7 + 3 = 20.                                     [Marks: 5]  ******/
+
+
+
+/**** Q#4: Given an integer N, the task is to find the sum of interior angles of an N-sided 
+            polygon.      	 	 	 	 	 	  [Marks: 5]  ****/ 
+
+
+    const polyBtn = document.getElementById("poly-btn");
+    polyBtn.addEventListener('click',sum);
+
+            function sum()
+            {
+                var n = document.getElementById("polygon").value;
+                if(n < 3) {
+                    alert('A Polygon has 3 or more sides');
+                    return false;
+                } else {
+                   var total = (n - 2) * 180;
+                }
+
+                document.getElementById('polygon-text').innerHTML = `The sum of the interior angles of the polygon is: ${total}`
+               
+            }
+             
+                
+/*** Q#5: Create a age calculator that takes date of birth of user and shows age in years, 
+months and days.                                 [Marks: 5]  ***/ 
 
